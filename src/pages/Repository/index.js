da238ef5e69783f18f4da549a94db37b1e45bae8
AkatGabrielGoncalves <gabrielmatheus9a@gmail.com>
2020-07-13 17:47:33 -0300

    feat: add prop-types, add repository api calls

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
 export default function Repository({ match }) {
-  return <h1>Repository: {decodeURIComponent(match.params.repo)}</h1>;
+  const [repoInfo, setRepoInfo] = useState({});
+  const [repoIssues, setRepoIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function getRepoInfo() {
+    const repoName = decodeURIComponent(match.params.repo);
+
+    const response = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    setRepoInfo(response[0].data);
+    setRepoIssues(response[1].data);
+
+    await setLoading(false);
+  }
+
+  useEffect(getRepoInfo, []);
+
+  return <h1>Repository</h1>;
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repo: PropTypes.string,
+    }),
+  }).isRequired,
+};
